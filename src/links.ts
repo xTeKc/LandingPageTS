d82e1cc014cf5c96606f27b2aa17d1fd9aaff154
xTeKc <bcd3v@pm.me>
2021-07-24 19:06:12 -0400

    add links file for socials

---
+++ src/links.ts
@@ -0,0 +1,6 @@
+
+export const dappUrl = 'https://app.yourDapp.com'
+export const discordUrl = 'https://discord.gg/yourDiscord'
+export const docsUrl = 'https://docs.yourDapp.com'
+export const githubUrl = 'https://github.com/yourGithub'
+export const twitterUrl = 'https://twitter.com/yourTwitter'
